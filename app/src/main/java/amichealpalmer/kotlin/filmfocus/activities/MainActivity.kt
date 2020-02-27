9c5342f9acfe4e13fbd8d178c690902bc9cc152f
ampalmer <am.palmer@outlook.com>
2020-02-27 17:09:33 +1300

    adding drawer UI with fragments

--- app/src/main/java/amichealpalmer/kotlin/filmfocus/activities/MainActivity.kt
+++ app/src/main/java/amichealpalmer/kotlin/filmfocus/activities/MainActivity.kt
@@ -1,9 +1,12 @@
 package amichealpalmer.kotlin.filmfocus.activities
 
 //import android.R
+
+import amichealpalmer.kotlin.filmfocus.R
 import amichealpalmer.kotlin.filmfocus.data.Film
 import amichealpalmer.kotlin.filmfocus.data.FilmThumbnail
-import amichealpalmer.kotlin.filmfocus.R
+import amichealpalmer.kotlin.filmfocus.fragments.BrowseFragment
+import amichealpalmer.kotlin.filmfocus.fragments.WatchlistFragment
 import android.app.SearchManager
 import android.content.ComponentName
 import android.content.Context
@@ -13,8 +16,15 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.SearchView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar.view.*
+
 
 // todo: see trello
 
@@ -25,14 +35,21 @@ class MainActivity : BaseActivity() {
     val testFilm = Film("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "")
     //private val search = Search(this) //
 
+    private lateinit var mDrawer: DrawerLayout
+    private lateinit var drawerToggle: ActionBarDrawerToggle
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        setSupportActionBar(drawer_layout.toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.setDisplayShowTitleEnabled(false)
+        mDrawer = findViewById(R.id.drawer_layout)
+        val nvDrawer = findViewById<NavigationView>(R.id.nvView)
+        setupDrawerContent(nvDrawer)
+
         Log.d(TAG, "Set content view done")
-        //   Log.d(TAG, "test call of FilmDetailsActivity")
-        // FilmDetailsActivity(testFilm)
 
         fab.setOnClickListener { view ->
             // todo action button
@@ -41,14 +58,46 @@ class MainActivity : BaseActivity() {
         }
 
         // test watchlist create
-        Log.d(TAG, ".onCreate: testing load of watchlist")
-        createTestWatchlist()
-
-        //Log.d(TAG, ".onCreate: load fragment into activity_main.xml")
-
+        //Log.d(TAG, ".onCreate: testing load of watchlist")
+        //createTestWatchlist()
 
         Log.d(TAG, ".onCreate finished")
     }
+
+
+    private fun setupDrawerContent(navigationView: NavigationView) {
+        Log.d(TAG, ".setupDrawerContent: setting nav view itemselectedlistener")
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            selectDrawerItem(menuItem)
+            true
+        }
+    }
+
+    fun selectDrawerItem(menuItem: MenuItem) { // Create a new fragment and specify the fragment to show based on nav item clicked
+        var fragment: Fragment? = null
+        val fragmentClass: Class<*>
+        fragmentClass = when (menuItem.itemId) {
+            R.id.nav_first_fragment -> BrowseFragment::class.java
+            R.id.nav_second_fragment -> WatchlistFragment::class.java
+            // R.id.nav_third_fragment -> ThirdFragment::class.java
+            else -> BrowseFragment::class.java
+        }
+        try {
+            fragment = fragmentClass.newInstance() as Fragment // not gonna work?
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        // Insert the fragment by replacing any existing fragment
+        val fragmentManager = supportFragmentManager
+        fragmentManager.beginTransaction().replace(R.id.main_frame_layout_fragment_holder, fragment!!).commit()
+        // Highlight the selected item has been done by NavigationView
+        menuItem.isChecked = true
+        // Set action bar title
+        title = menuItem.title
+        // Close the navigation drawer
+        mDrawer.closeDrawers()
+    }
+
 
     // todo: convert watchlist to fragment then test watchlist w/ this function
     fun createTestWatchlist() {
@@ -94,11 +143,17 @@ class MainActivity : BaseActivity() {
 
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //Log.d(TAG, ".onOptionsItemSelected triggered")
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            //R.id.action_settings -> true
+            android.R.id.home -> {
+                Log.d(TAG, ".onOptionsItemSelected: drawer button tapped")
+                mDrawer.openDrawer(GravityCompat.START)
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
