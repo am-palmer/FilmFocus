821c93775a79ad0734b5361c20bf2e3c14352482
unknown <am.palmer@outlook.com>
2020-03-14 19:42:41 +1300

    basic model for history timeline

---
+++ app/src/main/java/amichealpalmer/kotlin/filmfocus/data/TimelineItem.kt
@@ -0,0 +1,26 @@
+package amichealpalmer.kotlin.filmfocus.data
+
+import java.util.*
+
+// Holds FilmThumbnail, ID, Review, Star Rating, and Date marked watched. Displayed in the watched film timeline
+class TimelineItem(val film: FilmThumbnail, val rating: Int, val date: Date, private var review: String?) {
+
+    fun hasReview(): Boolean{
+        if (review != null){
+            return true
+        } else{
+            return false
+        }
+    }
+
+    fun getReview(): String{
+        if (hasReview()){
+            return review as String
+        } else return ""
+    }
+
+    fun setReview(review: String?){
+        this.review = review
+    }
+
+}
