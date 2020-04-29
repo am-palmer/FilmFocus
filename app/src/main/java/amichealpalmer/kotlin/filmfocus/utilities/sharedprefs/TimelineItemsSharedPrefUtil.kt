1387ada418e21270910ca5d41e139a3d0eb09123
unknown <am.palmer@outlook.com>
2020-04-29 17:23:45 +1200

    sharedpref utils for objects

---
+++ app/src/main/java/amichealpalmer/kotlin/filmfocus/utilities/sharedprefs/TimelineItemsSharedPrefUtil.kt
@@ -0,0 +1,38 @@
+package amichealpalmer.kotlin.filmfocus.utilities.sharedprefs
+
+import amichealpalmer.kotlin.filmfocus.model.FilmThumbnail
+import amichealpalmer.kotlin.filmfocus.model.TimelineItem
+import android.content.Context
+import android.util.Log
+import com.google.gson.reflect.TypeToken
+import java.lang.reflect.Type
+
+// Used to load and save the items displayed in the History timeline in SharedPrefs
+
+class TimelineItemsSharedPrefUtil(context: Context) : BaseSharedPrefUtil(context) {
+
+
+    private val TAG = "TimelineSharedPrefUt"
+    private val SHAREDPREFS_KEY_TIMELINEITEMS = "timelineItems"
+
+    // Returns either the timeline item array from SharedPrefs or null if it does not exist
+    fun loadTimelineItems(): ArrayList<TimelineItem>? {
+        Log.d(TAG, ".loadTimelineItems begins")
+        val timelineJson = sharedPreferences.getString(SHAREDPREFS_KEY_TIMELINEITEMS, null)
+        return if (timelineJson == null) {
+            null
+        } else {
+            val timelineType: Type = object : TypeToken<ArrayList<TimelineItem>>() {}.type
+            gson.fromJson(timelineJson, timelineType) as ArrayList<TimelineItem>
+        }
+    }
+
+    fun saveTimelineItems(watchlist: ArrayList<FilmThumbnail>) {
+        Log.d(TAG, ".saveTimelineItems begins")
+        val editor = sharedPreferences.edit()
+        val watchlistJson = gson.toJson(watchlist)
+        editor.putString(SHAREDPREFS_KEY_TIMELINEITEMS, watchlistJson)
+        editor.apply()
+    }
+
+}
--- app/src/main/java/amichealpalmer/kotlin/filmfocus/utilities/sharedprefs/WatchlistSharedPrefUtil.kt
+++ app/src/main/java/amichealpalmer/kotlin/filmfocus/utilities/sharedprefs/WatchlistSharedPrefUtil.kt
@@ -2,6 +2,7 @@ package amichealpalmer.kotlin.filmfocus.utilities.sharedprefs
 
 import amichealpalmer.kotlin.filmfocus.model.FilmThumbnail
 import android.content.Context
+import android.util.Log
 import com.google.gson.reflect.TypeToken
 import java.lang.reflect.Type
 
@@ -9,55 +10,29 @@ import java.lang.reflect.Type
 
 class WatchlistSharedPrefUtil(context: Context) : BaseSharedPrefUtil(context) {
 
-    private val TAG = "WatchlistStoreUtility"
+    private val TAG = "WatchlistSharedPrefUt"
     private val SHAREDPREFS_KEY_WATCHLIST = "watchlist"
 
+    // Returns either the watchlist from SharedPrefs or null if it does not exist
     fun loadWatchlist(): ArrayList<FilmThumbnail>? {
-        val gson = builder.create()
+        Log.d(TAG, ".loadWatchlist begins")
         val watchlistJson = sharedPreferences.getString(SHAREDPREFS_KEY_WATCHLIST, null)
-        val watchlistType: Type = object : TypeToken<ArrayList<FilmThumbnail>>() {}.type
-        return if (watchlistJson == null){
+        return if (watchlistJson == null) {
             null
         } else {
+            val watchlistType: Type = object : TypeToken<ArrayList<FilmThumbnail>>() {}.type
             gson.fromJson(watchlistJson, watchlistType) as ArrayList<FilmThumbnail>
         }
     }
 
     fun saveWatchlist(watchlist: ArrayList<FilmThumbnail>) {
+        Log.d(TAG, ".saveWatchlist begins")
+        val editor = sharedPreferences.edit()
+        val watchlistJson = gson.toJson(watchlist)
+        editor.putString(SHAREDPREFS_KEY_WATCHLIST, watchlistJson)
+
+        editor.apply()
 
     }
-
-//    private fun loadData() {
-//        Log.d(TAG, ".loadData called, loading data from Shared Preferences")
-//        val sharedPreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE)
-//        val builder = GsonBuilder()
-//        builder.registerTypeAdapter(LocalDate::class.java, LocalDateSerializer())
-//        // Watchlist
-//        val gson = builder.create()
-//
-//        // Load the watchlist
-//        val watchlistJson = sharedPreferences.getString(SHAREDPREFS_KEY_WATCHLIST, null)
-//        val watchlistType: Type = object : TypeToken<ArrayList<FilmThumbnail>>() {}.type
-//        watchlist = if (watchlistJson == null) { // Build a new watchlist
-//            Log.d(TAG, ".loadData: watchlist could not be loaded / doesn't exist yet. Making a new watchlist")
-//            ArrayList()
-//        } else {
-//            val watchlistRetrieved = gson.fromJson(watchlistJson, watchlistType) as ArrayList<FilmThumbnail>?
-//            Log.d(TAG, ".loadData: watchlist loaded. it contains ${watchlistRetrieved!!.size} items")
-//            watchlistRetrieved
-//        }
-//
-//        // Load the timeline items
-//        val timelineJson = sharedPreferences.getString(SHAREDPREFS_KEY_TIMELINE, null)
-//        val timelineType: Type = object : TypeToken<ArrayList<TimelineItem>>() {}.type
-//        timelineList = if (timelineJson == null) {
-//            Log.d(TAG, ".loadData: timeline could not be loaded / does not exist yet. Making a new watchlist")
-//            ArrayList()
-//        } else {
-//            val timelineRetrieved = gson.fromJson(timelineJson, timelineType) as ArrayList<TimelineItem>
-//            Log.d(TAG, ".loadData: timeline retrieved")
-//            timelineRetrieved
-//        }
-//    }
 
 }
